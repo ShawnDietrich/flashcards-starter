6e0e37137dde864923fca8e9acfd0a5429ed9542
Shawn <shawn.m.dietrich@gmail.com>
2022-03-24 20:47:26 -0400

    topics are being added to the state

--- src/features/topics/newTopics.js
+++ src/features/topics/newTopics.js
@@ -4,15 +4,44 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 //create slice
-export const topics = createSlice({
+export const topicSlice = createSlice({
   name: 'topics',
-  initialState: {topics: {}},
-  reducers: {
-    addTopic(state, action){
-      state.topics {
-        [id] = action.payload.id;
-
+  initialState: {
+    topics: {
+      '0': {
+        id: 0,
+        name: '',
+        icon: '',
+        quizIds: []
       }
+    }
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { uniqueId, name, icon } = action.payload;
+      const newTopic = {
+        id: uniqueId,
+        name: name,
+        icon: icon,
+        quizIds: []
+      }
+      state.topics = { ...state.topics, [uniqueId]: newTopic }
     }
   }
 })
+
+
+//selectors
+export const selectTopics = (state) => state.topics.topics;
+
+//reducers
+export const { addTopic } = topicSlice.actions;
+export default topicSlice.reducer;
+
+
+/*   let topicName = uniqueId;
+      topicName = Object.create(state.topics);
+      topicName.uniqueId = uniqueId;
+      topicName.name = name;
+      topicName.icon = icon;
+      topicName.quizIds = [];*/
